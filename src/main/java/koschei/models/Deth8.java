f97cf5767c55cd549f408fc33f4c39948a49b467
TIm IM <timimave@gmail.com>
2023-02-08 00:18:41 +0500

    MY_SOLUTION

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "хоть я и Кощей Бессмертный, но кто знает, может быть, "
+            + "это моя возможность получить наконец-то некоторый отдых!";
+    }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    @Autowired
+    public Needle7(Deth8 deth8) {
+        this.deth = deth8;
+    }
+
+    private final Deth8 deth;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth.toString();
     }
 }
